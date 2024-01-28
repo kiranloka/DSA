b42c71636ee463a7e02cb17b61551f4a6c3d1712
kiranloka <kirankumarloka@gmail.com>
2024-01-29 05:26:36 +0530

    reverse LL

--- reverseLink.java
+++ reverseLink.java
@@ -1,19 +1,78 @@
-class Node{
+class Node {
     int data;
     Node next;
 
-    Node(int data){
-        this.data=data;
-        this.next=null;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
-class LinkedList{
-    
+
+class LinkedList {
+    private Node head;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
 }
 
 public class reverseLink {
     public static void main(String[] args) {
-        
+        LinkedList linkedList = new LinkedList();
+
+        // Append elements to the linked list
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+        linkedList.append(4);
+        linkedList.append(5);
+
+        // Display the original linked list
+        System.out.println("Original linked list:");
+        linkedList.display();
+
+        // Reverse the linked list
+        linkedList.reverse();
+
+        // Display the reversed linked list
+        System.out.println("Reversed linked list:");
+        linkedList.display();
     }
-    
 }
